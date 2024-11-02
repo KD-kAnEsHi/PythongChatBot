b1d29f61ab334673b263742e7a745a717afba855
Lonely Man I AM <112342120+KD-kAnEsHi@users.noreply.github.com>
2024-11-02 01:34:00 -0500

    learning py in test file

---
+++ Python_Test/learn.py
@@ -0,0 +1,24 @@
+\
+
+# Exponents: 
+square = -2 ** -2 + 2 ** 4 # Such a simple way of squaring integers :) : 2 * 2 * 2 * 2 = 16.
+#print(square)
+
+
+# concatnation in Python
+string0 = "Good morning sir"
+string1 = "The wind blow souwest mr.so"
+sring2 = "The wind sure does blow from not only sound by also west"
+
+string3 = "Hey papa how come you work everyday and you cant feed me still?"
+numstostring = 10
+numsBeforeString = 9
+
+
+# Mutiple strings added
+# Going abooe amd he k, So many  
+
+
+#Playing the long game 13 mins amount
+while gphan >= 10:
+    print("Running the program now" + )
