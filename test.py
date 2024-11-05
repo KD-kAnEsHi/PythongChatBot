b24991dc0c451b830a854c6f27b4e86978253b5b
Lonely Man I AM <112342120+KD-kAnEsHi@users.noreply.github.com>
2024-11-05 16:44:57 -0600

    ch4 done

--- test.py
+++ test.py
@@ -3,7 +3,3 @@
 
 #There is no possible call that will make it return True as the keyword 'name' will always bind to the first parameter. For example:
 
-
-Lambda funtion (n):
-
-    return lambda X, X  pepper
