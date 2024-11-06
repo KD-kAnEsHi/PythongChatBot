abe2987a63b82491d2f63f9fd3a7b66164c8b406
Lonely Man I AM <112342120+KD-kAnEsHi@users.noreply.github.com>
2024-11-06 14:53:51 -0600

    ch4 done

--- Learning_Projects/test.py
+++ Learning_Projects/test.py
@@ -3,3 +3,9 @@
 
 #There is no possible call that will make it return True as the keyword 'name' will always bind to the first parameter. For example:
 
+import fibo
+
+print(fibo.fib(10))
+
+
+fibo.__name__
